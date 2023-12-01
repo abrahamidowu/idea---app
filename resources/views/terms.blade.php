906ccc4eb016853cc4efa4d80852735d60d3c1df
Abraham Idowu <idowuabraham1@gmail.com>
2023-12-01 18:53:31 +0100

    hi

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,20 @@
+@extends('layout')
+
+
+@section('layout')
+<h1>Term</h1>
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat officiis, officia voluptas reiciendis earum beatae cumque dolor repudiandae debitis, reprehenderit nam sit saepe commodi suscipit consequuntur. Nihil harum ipsum quod repellat officia sequi, ad blanditiis animi dolore, dolor incidunt nam?
+</p>
+@endsection
+
+<style>
+    h1{
+        text-align: center;
+    }
+
+    p{
+        text-align: justify;
+    }
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/" , function(){
+    return view("welcome");
+});
+
+Route::get("/terms" , function(){
+    return view("terms");
 });
